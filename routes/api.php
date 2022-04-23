2763261f54596dcfa70d8eeaab6421b943b5148f
LeonamJLima <leonam@puzl.place>
2022-04-23 11:21:42 -0300

    metodo para limpar cache via url

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Route;
 
 // Auth (Autenticação)
@@ -12,4 +13,18 @@ Route::group([
   Route::post('logout', 'AuthController@logout');
   Route::post('refresh', 'AuthController@refresh');
   Route::post('me', 'AuthController@me');
+});
+
+// Limpar cache
+Route::group([
+  'namespace' => 'App\Http\Controllers',
+  'middleware' => ['api', 'jwt', 'cors', 'acl', 'X-Locale'],
+], function () {
+  Route::get('/clear-cache', function () {
+    Artisan::call('cache:clear');
+    Artisan::call('route:clear');
+    Artisan::call('config:clear');
+    Artisan::call('view:clear');
+    return "Cache is cleared! You need to login again";
+  });
 });
